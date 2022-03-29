90fcc76e13c4ba9b449d8411ecf5ae64bc7daa8a
Umidjon <mrumidjon2020@gmail.com>
2022-03-29 14:39:58 +0500

    The s string variable is given. return all characters except n characters at the end. 01

--- Slicing07.py
+++ Slicing07.py
@@ -1,4 +1,4 @@
-def main(s,n):
+def main(s, n):
     """
     The s string variable is given. return all characters except n characters at the end.
     Args:
@@ -7,4 +7,4 @@ def main(s,n):
     Returns:
         str: answer
     """
-    return
+    return s[:-1 * n]
